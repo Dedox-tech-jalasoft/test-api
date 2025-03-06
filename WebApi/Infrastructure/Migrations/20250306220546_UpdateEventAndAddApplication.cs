46988577a22954497b66a79019f2cabaaa0fc6ba
Dedox-tech-jalasoft <diego.murillo@jalasoft.com>
2025-03-06 17:23:43 -0500

    Add latest migrations

---
+++ WebApi/Infrastructure/Migrations/20250306220546_UpdateEventAndAddApplication.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEventAndAddApplication : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AvailableTickets",
+                table: "Events",
+                newName: "TotalTicketsAssigned");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ApplicationId",
+                table: "Events",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Events",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Events",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Events",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateTable(
+                name: "Application",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    OwnerName = table.Column<string>(type: "text", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Application", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_ApplicationId",
+                table: "Events",
+                column: "ApplicationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Events_Application_ApplicationId",
+                table: "Events",
+                column: "ApplicationId",
+                principalTable: "Application",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Events_Application_ApplicationId",
+                table: "Events");
+
+            migrationBuilder.DropTable(
+                name: "Application");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Events_ApplicationId",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationId",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Events");
+
+            migrationBuilder.RenameColumn(
+                name: "TotalTicketsAssigned",
+                table: "Events",
+                newName: "AvailableTickets");
+        }
+    }
+}
